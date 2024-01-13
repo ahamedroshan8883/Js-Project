96538ddd4b317abe93a1cbd58d640b4fab80e158
ahamedroshan8883 <ahamed8883.h@gmail.com>
2024-01-13 12:56:11 +0530

    Made changes in Description Backgeround image

--- Hotel_Project/script.js
+++ Hotel_Project/script.js
@@ -1,40 +1,51 @@
 let dscrbdetaills = [
-                {
-                    title:"Suite Rooms & Dulex Rooms",
-                    img:"img/pngtree1.png",
-                    describe:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, mollitia inventore nostrum architecto labore ipsa nulla ducimus reiciendis id repellendus tempora earum numquam,fugiat saepe modi dolorem optio? Et, ipsam?"
-                },
-                {
-                    title:"Best Infrastructure",
-                    img:"img/images2.jpg",
-                    describe:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, mollitia inventore nostrum architecto labore ipsa nulla ducimus reiciendis id repellendus tempora earum numquam,fugiat saepe modi dolorem optio? Et, ipsam?"
-                },{
-                    title:"Great Hospitality",
-                    img:"img/pexels.jpg",
-                    describe:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, mollitia inventore nostrum architecto labore ipsa nulla ducimus reiciendis id repellendus tempora earum numquam,fugiat saepe modi dolorem optio? Et, ipsam?"
-                }
-            ]
-        let currentIndex = 0;
+    {
+        title:"Suite Rooms & Dulex Rooms",
+        img:"img/pngtree1.png",
+        describe:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, mollitia inventore nostrum architecto labore ipsa nulla ducimus reiciendis id repellendus tempora earum numquam,fugiat saepe modi dolorem optio? Et, ipsam?"
+    },
+    {
+        title:"Best Infrastructure",
+        img:"img/images2.jpg",
+        describe:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, mollitia inventore nostrum architecto labore ipsa nulla ducimus reiciendis id repellendus tempora earum numquam,fugiat saepe modi dolorem optio? Et, ipsam?"
+    },{
+        title:"Great Hospitality",
+        img:"img/pexels.jpg",
+        describe:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Vel, mollitia inventore nostrum architecto labore ipsa nulla ducimus reiciendis id repellendus tempora earum numquam,fugiat saepe modi dolorem optio? Et, ipsam?"
+    }
+]
+let currentIndex = 0;
 const slideprev = ()=>{
-    currentIndex--;
-    if(currentIndex<0){
-        currentIndex = dscrbdetaills.length-1}
-        console.log(currentIndex)
-        document.getElementById("title").textContent = dscrbdetaills[currentIndex].title
-        document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[currentIndex].img+")"
-        document.getElementById("dcsrbcon").textContent = dscrbdetaills[currentIndex].describe
+currentIndex--;
+if(currentIndex<0){
+currentIndex = dscrbdetaills.length-1}
+console.log(currentIndex)
+document.getElementById("title").textContent = dscrbdetaills[currentIndex].title
+document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[currentIndex].img+")"
+document.getElementById("dcsrbcon").textContent = dscrbdetaills[currentIndex].describe
 }
 const slidenext = ()=>{
-    currentIndex++;
-    if(currentIndex>dscrbdetaills.length-1){
-        console.log(currentIndex)
-        currentIndex = 0}
-        document.getElementById("title").textContent = dscrbdetaills[currentIndex].title
-        document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[currentIndex].img+")"
-        document.getElementById("dcsrbcon").textContent = dscrbdetaills[currentIndex].describe
+currentIndex++;
+if(currentIndex>dscrbdetaills.length-1){
+console.log(currentIndex)
+currentIndex = 0}
+document.getElementById("title").textContent = dscrbdetaills[currentIndex].title
+document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[currentIndex].img+")"
+document.getElementById("dcsrbcon").textContent = dscrbdetaills[currentIndex].describe
+}
+const slidenextpertime = ()=>{
+currentIndex++;
+if(currentIndex>dscrbdetaills.length-1){
+console.log(currentIndex)
+currentIndex = 0}
+document.getElementById("title").textContent = dscrbdetaills[currentIndex].title
+document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[currentIndex].img+")"
+document.getElementById("dcsrbcon").textContent = dscrbdetaills[currentIndex].describe
+setTimeout(slidenextpertime,10000)
 }
 window.addEventListener('DOMContentLoaded',function(){
-    document.getElementById("title").textContent = dscrbdetaills[0].title
-        document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[0].img+")"
-        document.getElementById("dcsrbcon").textContent = dscrbdetaills[0].describe
+document.getElementById("title").textContent = dscrbdetaills[0].title
+document.getElementById("imgcon").style.backgroundImage = "url("+dscrbdetaills[0].img+")"
+document.getElementById("dcsrbcon").textContent = dscrbdetaills[0].describe
+slidenextpertime()
 })
